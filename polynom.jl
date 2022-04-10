d848abf569a9d23cb0873f5c250955c42a5618c0
NickArhi <61687202+NickArhi@users.noreply.github.com>
2022-04-10 17:18:07 +0300

    Updated
    small changes in DFS_, added comments

--- polynom.jl
+++ polynom.jl
@@ -1,14 +1,12 @@
-using AbstractTrees
-using Graphs
 using Symbolics
 #= x' = exp(x+exp(x^2+x))=# 
 coun = 0
-@variables x 
-inp = []
-m = exp(x + exp(x+x^2))
-n = Symbolics.toexpr(m)
-k = Symbolics.operation(n)
-l = arguments(n)
+@variables t, x(t) 
+ex_var = []
+initial_expr = exp(x + exp(x+x^2)) + exp(x + x^2)
+expr_expr = Symbolics.toexpr(initial_expr)
+oper_expr = Symbolics.operation(expr_expr)
+arg_expr = arguments(expr_expr)
 
 
 
@@ -33,30 +31,45 @@ l = arguments(n)
 end=#
 
 
-function DFS_(l::Any, inp)
-    if (typeof(l) == Symbol) ||  (typeof(l) == Int64)
-        println(l)
+function DFS_(expression::Any, sub_list)                                      #Here we represent our equation as a tree and 
+    if (typeof(expression) == Symbol) ||  (typeof(expression) == Int64)     #search for correct substitutions
+        println(expression)
         return 0
     end
-    if typeof(l) == Num
-        println(l)
-    else typeof(l) == Int64 
-        println(l)
+    if typeof(expression) == Num
+        println(expression)
+    else typeof(expression) == Int64 
+        println(expression)
     end
-    line2 = operation(Symbolics.toexpr(l))
-    line = arguments(Symbolics.toexpr(l))
-    if (line2 == eval(exp))
-        push!(inp, eval(l))
+    line2 = operation(Symbolics.toexpr(expression))
+    line = arguments(Symbolics.toexpr(expression))
+    if (line2 == exp)
+        push!(sub_list, expression)
     end
     for i in line
-        DFS_(i, inp)
+        DFS_(i, sub_list)
     end
     return nothing
 end
   
-DFS_(m, inp)
+DFS_(initial_expr, ex_var)
+ex_var = unique(ex_var)
+print(ex_var)
 
-print(inp)
+
+....
+function Subst_(inp)
+    D = Differential(t)
+    for i in inp
+        println(i)
+        simplify(expand_derivatives(D(i)))
+        println(i)
+    end
+    return nothing
+end
+
+Subst_(ex_var)
+
 
 
 #=function operations(l::Any)
