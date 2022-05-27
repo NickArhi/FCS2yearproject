dae6fea0470a68616c439564adc1a9a9ccb75041
pogudingleb <pogudin.gleb@gmail.com>
2022-05-27 21:27:56 +0200

    Cleanup and restructuring

--- polynom.jl
+++ polynom.jl
@@ -1,19 +1,16 @@
 using Symbolics
 using SymbolicUtils
 using LinearAlgebra
-var_arr = @variables x, y
-initial_expr = [exp(x+y),exp(exp(x*y))]
-expr_expr = Symbolics.toexpr.(initial_expr)
 
-result = []
-substdict = Dict{Any, Any}()
-derivatives_subst_dict = Dict{Any, Any}()
+#------------------------------------------------------------------------------
 
 function normalization_of_z(dict)
     for (i,f) in enumerate(keys(dict))
         dict[f] = Symbol("z$i")
     end
 end
+
+#------------------------------------------------------------------------------
 
 function substsearch(substdict, expression)
     if typeof(expression) <: Int
@@ -30,21 +27,23 @@ function substsearch(substdict, expression)
     end
 end
 
+#------------------------------------------------------------------------------
 
-function Derivative_func(derivat::Any, initial_expr::Any, variables::Any) 
+function Derivative_func(derivat, initial_expr, variables) 
     result = []
-    k = length(initial_expr)-length(var_arr)
+    k = length(initial_expr) - length(variables)
     if k>0
         for i in 1:k
             push!(variables, 1)
         end
     end
     for i in derivat
-        push!(result, dot(Symbolics.gradient(eval(i),variables), eval.(initial_expr)))
+        push!(result, dot(Symbolics.gradient(eval(i), variables), eval.(initial_expr)))
     end
     return result
 end
 
+#------------------------------------------------------------------------------
 
 function substchange(expression, substdict)
     if typeof(expression) <: Int
@@ -54,27 +53,56 @@ function substchange(expression, substdict)
         return substdict[expression]
     end
     tree = [substchange(substitution, substdict) for substitution in arguments(expression)]
-    return Expr(:call, operation(expression), tree...)
+    return Expr(:call, Symbol(operation(expression)), tree...)
 end
 
-for i in expr_expr
-    substsearch(substdict, i)
+#------------------------------------------------------------------------------
+
+function print_ode(ode)
+    for (k, v) in ode
+        println("$k' = $(v)")
+    end
 end
 
-derivatives_subst = Derivative_func(keys(substdict), initial_expr, var_arr)
+#------------------------------------------------------------------------------
 
-for i in derivatives_subst
-    substsearch(derivatives_subst_dict, Symbolics.toexpr(i))
+"""
+    polynomialize(eqs)
+
+Takes as input a dictionary from state variables to their derivatives.
+Returns a new ODE system also represented as a dictionary and a dictionary expressing new
+variables in terms of the original ones
+"""
+function polynomialize(eqs)
+    vars = collect(keys(eqs))
+    rhs = collect(values(eqs))
+    expr_expr = Symbolics.toexpr.(rhs)
+    
+    result = []
+    substdict = Dict{Any, Any}()
+    derivatives_subst_dict = Dict{Any, Any}()
+
+    for i in expr_expr
+        substsearch(substdict, i)
+    end
+    
+    derivatives_subst = Derivative_func(keys(substdict), rhs, vars)
+    
+    for i in derivatives_subst
+        substsearch(derivatives_subst_dict, Symbolics.toexpr(i))
+    end
+    
+    normalization_of_z(derivatives_subst_dict)
+    derivatives_subst = Derivative_func(keys(derivatives_subst_dict), rhs, vars)
+    
+    for i in derivatives_subst
+        push!(result, substchange(Symbolics.toexpr(i), derivatives_subst_dict))
+    end
+    
+    return Dict(
+        :newvars => derivatives_subst_dict,
+        :new_ode => Dict(Symbol("z$i") => result[i] for i in 1:length(result))
+    )
 end
 
-normalization_of_z(derivatives_subst_dict)
-derivatives_subst_dict
-derivatives_subst = Derivative_func(keys(derivatives_subst_dict), initial_expr, var_arr)
-
-
-for i in derivatives_subst
-    push!(result,substchange(Symbolics.toexpr(i), derivatives_subst_dict))
-end
-
-derivatives_subst_dict 
-result
+#------------------------------------------------------------------------------
